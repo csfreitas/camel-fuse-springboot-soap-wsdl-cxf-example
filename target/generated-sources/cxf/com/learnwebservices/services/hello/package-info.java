c31e84ea7e2ce54259ecce6cc53d759d0578f51d
Cassio Augusto Silva de Freitas <kssiofreitas@gmail.com>
2022-01-26 20:35:44 -0300

    first commit

--- target/generated-sources/cxf/com/learnwebservices/services/hello/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://learnwebservices.com/services/hello", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.learnwebservices.services.hello;
